a17a43fbf780b7f71de2ab97241c7d3e3adac044
Dennie <dennievanderstarre@hotmail.com>
2018-06-22 18:59:42 +0200

    added fish

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,39 @@
-var a = 1;
-var b = true;
-a = 66;
-if (b == true) {
-    console.log(a);
+var cFish = (function () {
+    function cFish(_name, _position_x, _position_y, _hunger_level, _image) {
+        var _this = this;
+        this.draw = function () {
+            console.log(_this.image);
+            console.log(_this.position_x);
+            console.log(_this.position_y);
+            ctx.drawImage(_this.image, _this.position_x, _this.position_y, 20, 20);
+        };
+        this.swim = function () {
+        };
+        this.name = _name;
+        this.position_x = _position_x;
+        this.position_y = _position_y;
+        this.hunger_level = _hunger_level;
+        this.image = document.getElementById(_image);
+    }
+    return cFish;
+}());
+var canvas;
+var ctx;
+var fish_array = new Array();
+function game_loop() {
+    requestAnimationFrame(game_loop);
+    var fish;
+    for (var i = 0; i < fish_array.length; i++) {
+        fish = fish_array[i];
+        fish.draw();
+    }
 }
+window.onload = function () {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    fish_array.push(new cFish("Bob", 40, 20, 0, "gold-fish"));
+    fish_array.push(new cFish("Dave", 5, 15, 0, "gold-fish"));
+    fish_array.push(new cFish("Julia", 95, 30, 1, "gold-fish"));
+    game_loop();
+};
 //# sourceMappingURL=main.js.map
--- ts/game.ts
+++ ts/game.ts
@@ -1,8 +1,25 @@
-let a = 1
-let b = true
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D; 
+let fish_array: Array<cFish> = new Array<cFish>();
 
-a = 66
+function game_loop() {
+    requestAnimationFrame(game_loop);
 
-if( b == true ) {
-    console.log( a )
+    let fish:cFish;
+    for(let i:number = 0; i < fish_array.length; i++) {
+        fish = fish_array[i];
+        fish.draw();
+        //fish.swim();
+    }
+}
+
+window.onload = () => {
+    canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+
+    fish_array.push(new cFish("Bob", 40, 20, 0, "gold-fish"));
+    fish_array.push(new cFish("Dave", 5, 15, 0, "gold-fish"));
+    fish_array.push(new cFish("Julia", 95, 30, 1, "gold-fish"));
+
+    game_loop();
 }
